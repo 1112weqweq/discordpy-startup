07ace61fdfa7f4b49d3fa7d0178a27f68984d6e4
1112weqweq <100444731+1112weqweq@users.noreply.github.com>
2022-02-26 07:13:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@
 import discord
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'OTQ2ODY2OTMzNzcxNDM2MTQy.Yhk8jA.n2d9jGurjBUcNJkMEkewuuD9Lxg'
+TOKEN = 'OTQ2ODY2OTMzNzcxNDM2MTQy.Yhk8jA.AdGAX9LgsLWR2_7kCjiUHZOdqk0'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
